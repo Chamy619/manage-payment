dff361b2e1e5d2dde9379ad2e4721697a7990dbe
Chamy <yd95069506@naver.com>
2021-09-05 17:24:13 +0900

    :sparkles: 성과급 입력하지 않았을 때 0으로 처리

--- src/lib/calculate.js
+++ src/lib/calculate.js
@@ -1,10 +1,10 @@
-export const calculate = (payment, pi, ps, bonus, ratio, year) => {
+export function calculate(payment, pi, ps, bonus, ratio, year) {
   const PAYMENT = parseInt(payment);
   const month = year * 12;
-  const PI = pi * 2;
-  const PS = parseInt(ps);
-  const BONUS = bonus * 2;
-  const RATIO = 1 + parseInt(ratio) / 100;
+  const PI = !pi ? 0 : pi * 2;
+  const PS = !ps ? 0 : parseInt(ps);
+  const BONUS = !bonus ? 0 : bonus * 2;
+  const RATIO = !ratio ? 1 : 1 + parseInt(ratio) / 100;
   let result = 0;
 
   for (let m = 0; m < month; m++) {
@@ -20,4 +20,4 @@ export const calculate = (payment, pi, ps, bonus, ratio, year) => {
   result *= 10000;
 
   return parseInt(result).toLocaleString();
-};
+}
